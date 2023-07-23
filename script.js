9a03fcdd7417ece1fd8ce549b8bedf6f06cbc79c
Shinigami2222 <72208090+Shinigami2222@users.noreply.github.com>
2023-07-23 22:33:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// JavaScript function to show the loading modal
 function showLoadingModal() {
   document.getElementById("loadingModal").style.display = "flex";
 }
-// JavaScript function to hide the loading modal
+
 function hideLoadingModal() {
   document.getElementById("loadingModal").style.display = "none";
 }
